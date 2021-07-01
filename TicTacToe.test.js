1a4ffa983b5d9fcd143662a13416132ad61fa2ed
Nadia Rosenberg <nadia>
2021-07-01 20:57:54 -0300

    Adds test for X wins and O wins

--- TicTacToe.test.js
+++ TicTacToe.test.js
@@ -9,7 +9,7 @@ test('The board is not yet finished', () => {
   expect(isSolved(board)).toBe(-1);
 });
 
-test('X wins', () => {
+test('X wins, horizontal win', () => {
   board = [
     [1,1,1],
     [1,0,2],
@@ -18,11 +18,65 @@ test('X wins', () => {
   expect(isSolved(board)).toBe(1);
 });
 
-test('O wins', () => {
+test('X wins, vertical win', () => {
+  board = [
+    [2,1,0],
+    [1,1,2],
+    [2,1,0]
+  ]
+  expect(isSolved(board)).toBe(1);
+});
+
+test('X wins, main diagonal win', () => {
+  board = [
+    [1,1,0],
+    [2,1,2],
+    [2,1,1]
+  ]
+  expect(isSolved(board)).toBe(1);
+});
+
+test('X wins, secondary diagonal win', () => {
+  board = [
+    [2,0,1],
+    [2,1,2],
+    [1,2,1]
+  ]
+  expect(isSolved(board)).toBe(1);
+});
+
+test('O wins, horizontal win', () => {
+  board = [
+    [2,2,2],
+    [1,2,1],
+    [1,1,0]
+  ]
+  expect(isSolved(board)).toBe(2);
+});
+
+test('O wins, vertical win', () => {
+  board = [
+    [1,1,2],
+    [1,2,2],
+    [0,1,2]
+  ]
+  expect(isSolved(board)).toBe(2);
+});
+
+test('O wins, main diagonal win', () => {
   board = [
     [2,1,1],
     [1,2,2],
     [2,1,2]
+  ]
+  expect(isSolved(board)).toBe(2);
+});
+
+test('O wins, secondary diagonal win', () => {
+  board = [
+    [1,1,2],
+    [1,2,2],
+    [2,0,1]
   ]
   expect(isSolved(board)).toBe(2);
 });
